524c53f6c3b12e86402d3814820a7cc69b39440a
PeCMay2105 <pedrocmaia2105@gmail.com>
2023-05-30 07:05:28 -0300

    botao e alterações nos inputs

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,10 +1,12 @@
 import './ListaSuspensa.css'
 const ListaSuspensa = (props) =>{
 const lista = props.lista
+const label = props.label
     return(
-        <div>
+        <div className='listaSuspensa'>
+            <h3>{label}</h3>
             <select>
-                {lista.map(item => <option>{item}</option>)}
+                {lista.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
     )
--- src/componentes/formulario/index.js
+++ src/componentes/formulario/index.js
@@ -1,17 +1,23 @@
 import './formulario.css'
 import CamposTexto from '../camposFormulario'
 import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../botao'
 
 const Formulario = () => {
-
+const escutador = (event) =>{
+    event.preventDefault();
+    console.log("Informações enviadas")
+}
+const listaDeTimes = ["Programação","Front-end","Data-Science","Dev-ops","Ux e Design","Mobilie","Inovação e Gestão"]
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={escutador}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CamposTexto label="Nome" placeholder="Digite seu nome"></CamposTexto>
                 <CamposTexto label="Cargo" placeholder="Digite seu cargo"></CamposTexto>
                 <CamposTexto label="Imagem" placeholder="Insira o endereço da imagem"></CamposTexto>
-                <ListaSuspensa lista={["Programação","Front-end","Data-Science","Dev-ops"]}></ListaSuspensa>
+                <ListaSuspensa lista={listaDeTimes} label="Escolha seu time"></ListaSuspensa>
+                <Botao conteudo="Criar card">Criar card</Botao>
             </form>
         </section>
     )
